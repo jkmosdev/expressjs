4f4e49fee85ce24c5f15238dadf0fd704223e4cc
James <jkmos.developer@gmail.com>
2020-10-11 17:30:58 +0800

    hello world
    -- added nodemon

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
 app.listen(PORT, () => console.log(`listening to port ${PORT}...`));
